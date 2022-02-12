90a6477743ee32864a26d5fe57fd04c73388b947
Prabhu7777 <99548346+Prabhu7777@users.noreply.github.com>
2022-02-12 21:23:26 +0530

    Week 1 assignment q1

---
+++ q1.c
@@ -0,0 +1,21 @@
+// Q1
+#include<stdio.h>
+int main()
+{
+        char n[40],b[40],h[100];
+        int r;
+        printf("\nName:\n");
+        scanf("%s",&n);
+        printf("\nBranch:\n");
+        scanf("%s",&b);
+        printf("\nHobbies:\n");
+        scanf("%s",&h);
+        printf("\nRoll no.:\n");
+        scanf("%d",&r);
+        printf("\nName: %s",n);
+        printf("\nBranch: %s",b);
+        printf("\nHobbies: %s",h);
+        printf("\nRoll no.: %d",r);
+        return 0;
+        
+}
